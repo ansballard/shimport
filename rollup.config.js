75ece84bff5842208a0fd62b4eb82861f6cb994e
Aaron Ballard <ans.ballard@gmail.com>
2020-09-21 21:13:28 -0400

    added a typescript es5 build and the browser field in package.json to export it

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,24 @@
 import sucrase from '@rollup/plugin-sucrase';
+import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-const config = dev => ({
+const config = (dev, es5 = false) => ({
 	input: 'src/index.ts',
 	output: [
 		{
-			file: dev ? 'index.dev.js' : pkg.main,
+			file: dev ? 'index.dev.js' : es5 ? pkg.browser : pkg.main,
 			format: 'iife',
 			name: '__shimport__'
 		}
 	],
 	plugins: [
-		sucrase({ transforms: ['typescript'] }),
+		!es5
+			? sucrase({ transforms: ['typescript'] })
+			: typescript({
+				target: 'es5'
+			}),
 		replace({
 			__VERSION__: pkg.version
 		}),
@@ -23,5 +28,6 @@ const config = dev => ({
 
 export default [
 	config(false),
-	config(true)
+	config(true),
+	config(false, true)
 ];
